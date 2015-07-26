e347a26d3e327dc7edbdc4e9c9f0c3fc503e5ec5
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2015-07-26 14:47:19 -0500

    [ADD]: OML7

--- oml7/files/entrypoint.sh
+++ oml7/files/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/opt/odoo/8.0/odoo.py \
--c /opt/odoo/config/odoo.conf \
+/opt/odoo/7.0/openerp-server \
+-c /opt/odoo/odoo-server.conf \
 --db_user=$DB_ENV_POSTGRES_USER \
 --db_password=$DB_ENV_POSTGRES_PASSWORD \
 --db_host=$DB_PORT_5432_TCP_ADDR \
