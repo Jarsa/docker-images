af70e8df704e325c04159f62a8f02547126fbb17
Luis Triana <luistriana.28@gmail.com>
2016-02-16 17:35:34 -0600

    [FIX]-Changes in the Docker file and delete dependencies file for Odoo 7

--- odoo-7/files/entrypoint.sh
+++ odoo-7/files/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 /opt/odoo/7.0/openerp-server \
--c /opt/odoo/odoo.conf \
---db_user=$DB_ENV_POSTGRES_USER \
---db_password=$DB_ENV_POSTGRES_PASSWORD \
---db_host=$DB_PORT_5432_TCP_ADDR \
---db-filter=^%h$ \
---proxy-mode
+-c /opt/odoo/odoo-server.conf \
+--db_host=$DB_PORT_5432_TCP_ADDR 
 
 exec "$@"
