d56c86cb0a28b0ff46d01ec1aa586df440b949be
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2016-03-10 19:13:52 -0600

    [FIX] change entrypoint to remove backslash

--- odoo-8/files/entrypoint.sh
+++ odoo-8/files/entrypoint.sh
@@ -3,6 +3,6 @@
 -c /opt/odoo/odoo-server.conf \
 --db_user=$DB_ENV_POSTGRES_USER \
 --db_password=$DB_ENV_POSTGRES_PASSWORD \
---db_host=$DB_PORT_5432_TCP_ADDR \
+--db_host=$DB_PORT_5432_TCP_ADDR
 
 exec "$@"
